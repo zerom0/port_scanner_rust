befd46cefd5f54eabdab9122d542c37cf2e20c7a
Martin Mosler <martin.mosler@zuehlke.com>
2022-12-20 08:46:28 +0100

    v2 - command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
+use std::env::args;
 use std::net::{SocketAddr, SocketAddrV4, TcpStream};
 use std::str::FromStr;
 use std::time::Duration;
 
 fn main() {
-    let address = SocketAddr::from(SocketAddrV4::from_str("127.0.0.1:80").unwrap());
+    let args: Vec<String> = args().collect();
+    if args.len() != 4 || args[2] != "-s" {
+        panic!("Invalid arguments")
+    }
+
+    let address_string = format!("{}:{}", args[1], args[3]);
+    let address_v4 = match SocketAddrV4::from_str(&address_string) {
+        Ok(x) => x,
+        Err(_) => panic!("Invalid address {}", address_string),
+    };
+    let address = SocketAddr::from(address_v4);
     let timeout = Duration::from_secs(3);
-    let result = TcpStream::connect_timeout(&address, timeout);
-    println!("{:?}", result);
+
+    match TcpStream::connect_timeout(&address, timeout) {
+        Ok(_) => println!("Port {} is open ✅", address_v4.port()),
+        _ => println!("Port {} is NOT open 🛑", address_v4.port()),
+    }
 }
