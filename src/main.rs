84a63b06819caff4fe5291ddaa1bd2766a12c01a
Martin Mosler <martin.mosler@zuehlke.com>
2022-12-20 08:23:45 +0100

    v1 - scan single port

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::net::{SocketAddr, SocketAddrV4, TcpStream};
+use std::str::FromStr;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let address = SocketAddr::from(SocketAddrV4::from_str("127.0.0.1:80").unwrap());
+    let timeout = Duration::from_secs(3);
+    let result = TcpStream::connect_timeout(&address, timeout);
+    println!("{:?}", result);
 }
